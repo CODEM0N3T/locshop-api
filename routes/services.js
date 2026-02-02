a73b2d54a5deaf8817215b425197d0dd3621cfb7
Monet <codem0n3t@gmail.com>
2026-02-02 02:03:56 -0500

    Added error handling, try/catch, finished servicePOST and GET requests, started appointments model

--- routes/services.js
+++ routes/services.js
@@ -2,12 +2,24 @@ const Service = require("../models/service");
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
-  const services = await Service.find();
-  res.send(services);
+  try {
+    const services = await Service.find();
+    res.status(200).json(services);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-router.post("/", (req, res) => {
-  res.send("POST request to the homepage");
+router.post("/", async (req, res) => {
+  try {
+    const serviceData = req.body; //JSON body
+    const newService = new Service(serviceData); //create new service
+    const savedService = await newService.save(); //save service to db
+    res.status(201).json(savedService); //return/respond with the created document and 201 status
+  } catch (error) {
+    res.status(400).json({ message: error.message }); //handle errors
+  }
+
 });
 
 module.exports = router;
